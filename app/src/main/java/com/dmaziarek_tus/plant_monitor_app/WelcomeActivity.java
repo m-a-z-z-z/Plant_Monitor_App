ab41a01aa11703888f3f2c16d036f148c25e7fa3
__Mazzz__ <dylanmaz@protonmail.com>
2023-02-12 21:27:29 +0000

    Updated google services to resolve firebase bug

--- app/src/main/java/com/dmaziarek_tus/plant_monitor_app/WelcomeActivity.java
+++ app/src/main/java/com/dmaziarek_tus/plant_monitor_app/WelcomeActivity.java
@@ -6,12 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        FirebaseApp.initializeApp(this);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 
     public void onButtonSignUpClicked(View view) {
